e33a2c7e25985282fe2dcb9cab7f70dc69515944
Deva Brahmasta <devabrahmasta24@gmail.com>
2025-09-06 23:35:02 +0700

    add log in page

--- renewme/lib/main.dart
+++ renewme/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:renewme/view/home_page.dart';
+import 'package:renewme/view/login_page/login_page.dart';
 import 'firebase_options.dart';
 import 'package:get/get.dart';
 void main() async {
@@ -22,7 +23,7 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const LoginPage(),
     );
   }
 }
