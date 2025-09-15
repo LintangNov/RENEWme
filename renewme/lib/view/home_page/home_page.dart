b717752172e6206fa1c5d7d36757bdf33ed9c6ab
Deva Brahmasta <devabrahmasta24@gmail.com>
2025-09-15 14:16:40 +0700

    feat: enhance food card display with dynamic location fetching and improved UI elements

--- renewme/lib/view/home_page/home_page.dart
+++ renewme/lib/view/home_page/home_page.dart
@@ -383,8 +383,14 @@ class _HomePageState extends State<HomePage> {
                                         ),
                                         borderRadius: BorderRadius.circular(10),
                                       ),
-                                      child: Text(foodController.getFoodDistanceString(foodData)),
+                                      child: Text(
+                                        foodController.getFoodDistanceString(
+                                          foodData,
+                                     s   ),
+                                      ),
                                     ),
+                                    SizedBox(height: 20),
+                                    Text('Pengambilan Hari ini,'),
                                     SizedBox(height: 20),
                                     SizedBox(
                                       height: 40,
@@ -432,8 +438,123 @@ class _HomePageState extends State<HomePage> {
                                       height: 400,
                                     ), // Ini hanya untuk simulasi konten panjang
                                     Text("Konten paling bawah."),
+                                    Container(
+                                      margin: EdgeInsets.symmetric(
+                                        horizontal: 20,
+                                        vertical: 20,
+                                      ),
+                                      height: 220,
+                                      width: double.infinity,
+                                      child: InkWell(
+                                        onTap: () {
+                                          print('tap');
+                                        },
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                              color: Colors.grey.shade300,
+                                              width: 2,
+                                            ),
+                                            borderRadius: BorderRadius.circular(
+                                              20,
+                                            ),
+                                          ),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              //image
+                                              ClipRRect(
+                                                borderRadius:
+                                                    const BorderRadius.only(
+                                                      topLeft: Radius.circular(
+                                                        20,
+                                                      ),
+                                                      topRight: Radius.circular(
+                                                        20,
+                                                      ),
+                                                    ),
+                                                child: Image.network(
+                                                  foodData.imageUrl.toString(),
+                                                  height: 150,
+                                                  width: double.infinity,
+                                                  fit: BoxFit.cover,
+                                                  loadingBuilder:
+                                                      (
+                                                        context,
+                                                        child,
+                                                        progress,
+                                                      ) =>
+                                                          progress == null
+                                                              ? child
+                                                              : const SizedBox(
+                                                                height: 150,
+                                                                child: Center(
+                                                                  child:
+                                                                      CircularProgressIndicator(),
+                                                                ),
+                                                              ),
+                                                  errorBuilder:
+                                                      (
+                                                        context,
+                                                        error,
+                                                        stack,
+                                                      ) => Image.asset(
+                                                        'assets/images/food_loading_image.png',
+                                                        height: 150,
+                                                        width: double.infinity,
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                ),
+                                              ),
+                                              //nama jalan
+                                              Padding(
+                                                padding: const EdgeInsets.all(
+                                                  10,
+                                                ),
+                                                child: Expanded(
+                                                  child: Obx(() {
+                                                    if (userController
+                                                            .isFetchingLocation
+                                                            .value ||
+                                                        userController
+                                                            .isFetchingAddress
+                                                            .value) {
+                                                      return const Text(
+                                                        'Mencari lokasi anda...',
+                                                        style: TextStyle(
+                                                          color: Colors.white,
+                                                          fontWeight:
+                                                              FontWeight.bold,
+                                                          fontSize: 12,
+                                                        ),
+                                                      );
+                                                    }
+                                                    return Text(
+                                                      userController
+                                                          .userAddress
+                                                          .value,
+                                                      style: const TextStyle(
+                                                        color: Colors.white,
+                                                        fontWeight:
+                                                            FontWeight.bold,
+                                                        fontSize: 12,
+                                                      ),
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      maxLines: 1,
+                                                    );
+                                                  }),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
                                   ],
                                 ),
+                                SizedBox(height: 120),
                               ],
                             ),
                           ),
@@ -442,13 +563,13 @@ class _HomePageState extends State<HomePage> {
                       Align(
                         alignment: Alignment.bottomCenter,
                         child: Container(
+                          padding: EdgeInsets.fromLTRB(20, 0, 20, 20),
                           height: 100,
                           width: double.maxFinite,
                           // alignment: Alignment.bottomCenter,
                           decoration: BoxDecoration(color: Colors.amber),
                           child: Column(
                             children: [
-
                               Container(
                                 // margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                                 width: double.infinity,
